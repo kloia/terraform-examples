80ad81b0d0cfc5b8c9cd8dadf64718c5032d9076
Emir Özbir <emirozbir@kloia.com>
2020-05-21 15:39:54 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,16 @@
 variable "ami_id" {
   default = "ami-07c1207a9d40bc3bd"
+  description = "ID of AMI that use to for EC2 instance"
 }
 
 variable "instance_type" {
   default = "t2.micro"
+  description = "Type of EC2 instance"
+  
 }
 
 variable "region" {
   default = "us-east-2"
+  description = "Region of where to apply your terraform implementations"
+
 }
